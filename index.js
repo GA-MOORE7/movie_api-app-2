62f3a2ac7401192a709136d733740304e3183419
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-09-24 00:28:53 +0200

    Update app.use(cors());

--- index.js
+++ index.js
@@ -30,16 +30,18 @@ app.use(bodyParser.urlencoded({ extended: true}));
 // Restrict requests to specific origins
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://myflix-app-gamoore.netlify.app'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  }));
+app.use(cors(
+//     {
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//         let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   }
+  ));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
